901ccf9515ef5b64ee29dbdc60b9a7843581466c
Josiah McPeak <mcpeakjosiah@gmail.com>
2018-05-02 17:19:25 -0700

    added math module assignment

---
+++ math_module/app.js
@@ -0,0 +1,13 @@
+var mathlibrary = require('./mathlibrary');
+
+const test1 = mathlibrary.add(3,5);
+console.log(test1);
+
+const test2 = mathlibrary.multiply(143252,555);
+console.log(test2);
+
+const test3 = mathlibrary.square(12343432256);
+console.log(test3);
+
+const test4 = mathlibrary.random(144,5344);
+console.log(test4);
